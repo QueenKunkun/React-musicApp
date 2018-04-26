bd96a34b2533da6eabef0b691551b379d52b331e
zhangkun <zhangkun@bimsop.com>
2018-04-26 14:00:55 +0800

    增加播放控制页面和歌曲列表页面以及列表组件

--- app/root.js
+++ app/root.js
@@ -1,53 +1,45 @@
 import React from 'react';
 import Header from './components/header';
-import Progress from './components/progress'
+import Player from './page/player';
+import MusicList from './page/musicList';
+import { MUSIC_LIST } from './config/musicliat';
 
-let duration = null;
 let Root = React.createClass({
   getInitialState() {
+    console.log('MUSIC_LIST', MUSIC_LIST);
     return {
-      progress: '-'
-    }
+      musicList: MUSIC_LIST,
+      currentMusicItem: MUSIC_LIST[0],
+    };
   },
   componentDidMount() {
     $('#player').jPlayer({
-      ready: function () {
+      ready: function() {
         console.log('1111');
         $(this)
           .jPlayer('setMedia', {
-            mp3: 'http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.mp3'
-            // mp3: 'http://pic.ibaotu.com/00/58/90/86y888piCftQ.mp3'
+            // mp3: 'http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.mp3'
+            mp3: 'http://pic.ibaotu.com/00/58/90/86y888piCftQ.mp3'
           })
           .jPlayer('play');
       },
       supplied: 'mp3',
       wmode: 'window'
     });
-    $('#player').bind($.jPlayer.event.timeupdate, (e) => {
-      duration = e.jPlayer.status.duration;
-      this.setState({
-        progress: Math.round(e.jPlayer.status.currentPercentAbsolute)
-      });
-    });
-  },
-  componentWillUnMount() {
-    $('#player').unbind($.jPlayer.event.timeupdate);
-  },
-  progressChangeHandler(progress) {
-    console.log('progressChangeHandler', progress);
-    $('#player').jPlayer('play', duration * progress);
   },
   render() {
-    return(
+    return (
       <div>
         <Header />
-        <Progress
-          progress={this.state.progress}
-          onProgressChange={this.progressChangeHandler}
-          barColor="#ff0000"
+        {/* <Player
+        currentMusicItem={this.state.currentMusicItem}/>
+        <div id="player" /> */}
+        <MusicList
+          currentMusicItem={this.state.currentMusicItem}
+          musicList={this.state.musicList}
         />
-        <div id="player" />
-    </div>);
+      </div>
+    );
   }
 });
 
